8cf84ade17b81f5b142dbf590e67aea485c7cb2c
James Halliday <mail@substack.net>
2013-09-08 14:51:53 +0100

    browser section of hyperkey track works

--- hyperkey/410_browser.js
+++ hyperkey/410_browser.js
@@ -3,3 +3,7 @@ var sock = shoe('/sock');
 
 var render = require('./050_render.js')();
 render.pipe(sock).pipe(render.sortTo('#messages'));
+
+// browserify -t brfs 410_browser.js -o static/bundle.js
+// node 320_server.js &
+// xdg-open http://localhost:5000
