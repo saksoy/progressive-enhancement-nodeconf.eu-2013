7cd5e2c780dc8da45917149714de74054e67cee6
James Halliday <mail@substack.net>
2013-09-10 10:17:12 +0100

    fix rendering require()

--- assoc/340_hackerspace.js
+++ assoc/340_hackerspace.js
@@ -1,7 +1,7 @@
 var hyperkey = require('hyperkey');
 var fs = require('fs');
 var html = fs.readFileSync(__dirname + '/300_hackerspace.html');
-var hacker = require('./hacker.js');
+var hacker = require('./420_hacker.js');
 
 module.exports = function () {
     return hyperkey(html, function (row) {
